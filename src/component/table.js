e81db53895045a7eda104284e358d553e041f790
DewaBiara <angabiara@gmail.com>
2023-12-06 11:46:33 +0800

    Update

--- src/component/table.js
+++ src/component/table.js
@@ -1,42 +1,49 @@
+import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 
-function DTable(){
-  const tableStyle = {
-    width: '100%', // Menetapkan lebar tabel menjadi 100%
-    height: '300px', // Menetapkan tinggi tabel, sesuaikan dengan kebutuhan Anda
-};
-    return(
-        <Table striped bordered hover size="sm" style={tableStyle}>
-        <thead>
-          <tr>
-            <th>Time</th>
-            <th>Temperature</th>
-            <th>Humidity</th>
-            <th>Luminous</th>
+function DTable() {
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    // Fungsi utilitas untuk mendapatkan data dari API lokal
+    const fetchData = async () => {
+      try {
+        // Ganti dengan URL atau fungsi untuk mengambil data lokal
+        const response = await fetch('http://localhost:8080/api/data'); // Contoh URL API lokal
+        const data = await response.json();
+        setTableData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Panggil fungsi utilitas untuk memuat data saat komponen dimuat
+    fetchData();
+  }, []); // Efek hanya dijalankan saat komponen di-mount
+  
+
+  return (
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Time</th>
+          <th>Temperature</th>
+          <th>Humidity</th>
+          <th>Luminous</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData.map((rowData, index) => (
+          <tr key={index}>
+            <td>{rowData.CreatedAt}</td>
+            <td>{rowData.Temperature}</td>
+            <td>{rowData.Humidity}</td>
+            <td>{rowData.Light}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td ></td>
-          </tr>
-        </tbody>
-      </Table>
-    )
+        ))}
+      </tbody>
+    </Table>
+  );
 }
 
 export default DTable;
